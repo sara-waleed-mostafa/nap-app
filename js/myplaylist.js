5f51fb2d53f698f354213c0264c913d61fc600a2
sara-waleed-mostafa <sara.waleed.mostafa267@gmail.com>
2017-07-11 18:58:56 +0200

    nap app project final iti

---
+++ js/myplaylist.js
@@ -0,0 +1,18 @@
+
+
+var myPlaylist = [
+
+    {
+        mp3:'mix/1.mp3',
+        oga:'mix/1.ogg',
+//        mp3:'mix/2.mp3',
+//        oga:'mix/2.ogg',
+        title:'Sample',
+        artist:'Sample',
+        rating:4,
+        buy:'#',
+        price:'0.99',
+        duration:'0:30',
+        cover:'mix/1.png'
+    }
+];
---
+++ js/newjavascript.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    $("#a").click(function(){
+        $("#v").slideToggle("slow");
+    });
+  
+    $("#s").click(function(){
+        $("#ss").slideToggle("slow");
+    });
+    $("#y").click(function(){
+        $("#yy").slideToggle("slow");
+    });
+    $("#n").click(function(){
+        $("#nn").slideToggle("slow");
+    });
+    $("#f").click(function(){
+        $("#ff").slideToggle("slow");
+    });
+     $("#oo").click(function(){
+        $("#ooo").slideToggle("slow");
+    });
+     $("#h").click(function(){
+        $("#hh").slideToggle("slow");
+    });
+    $("#w").click(function(){
+        $("#ww").slideToggle("slow");
+    });
+     $("#u").click(function(){
+        $("#uuu").slideToggle("slow");
+    });
+     $("#t").click(function(){
+        $("#tt").slideToggle("slow");
+    });
+     $("#c").click(function(){
+        $("#cc").slideToggle("slow");
+    });
+     $("#e").click(function(){
+        $("#ee").slideToggle("slow");
+    });
+     $("#j").click(function(){
+        $("#jj").slideToggle("slow");
+    });
+     $("#o").click(function(){
+        $("#muppetss").animate({
+              left: '250px',
+            height: '350px',
+            width: '550px'
+        });
+        });
+       $("#o").click(function(){
+        $("#m").show();
+        });
+        
+   
+});
+
+	  
